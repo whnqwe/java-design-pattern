2383dbdff6f41e3e260ce4e1df498015267d0ed6
zhanghui <whnqwe@126.com>
2019-01-08 15:20:41 +0800

    first commit

---
+++ src/state/HomeContext.java
@@ -0,0 +1,19 @@
+package state;
+
+
+/**
+ * 房间对象
+ * @author Administrator
+ *
+ */
+public class HomeContext {
+	private State state;
+	
+	
+	public void setState(State s){
+		System.out.println("修改状态！");
+		state = s;
+		state.handle();
+	}
+	
+}
